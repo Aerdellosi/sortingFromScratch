5474319e51b6efee63eaac87dc77b9299d56ef6a
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-01-18 23:03:25 -0800

    initial commit with js algorithm

---
+++ ghetto_search.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    //1) create two arrays, one filled with words and the other to be
+    //populated (A and B)
+
+    //2) Sort for unique words using a fill check loop, fill B
+
+    //3) make a repeat counter
+    //append repeats count to a new array C
+
+    //4) create variable highest number
+
+    //5) for values in C, if element === highestNumber,
+    //highestnumber = element
+
+    //6) for (let i = highestNumber; i > 0; i--)
+    //move through C, if element === i, push to new array D
+
+    //7) create two new elements D and F
+    //for array D, move through and push C[D] to array E
+    //and push B[D] to F
+
+    //8) append each matching key/value together in a string
+    //with key/value notation in final array G
+    //for (i = 0; i < arrayE.length; i++) {
+    //  array G push E[i] + ": " + F[i];   
+    //}
+
+    //9) Ui logic follows ....
+
+
+
+
+
+
+
+
+
+}
